50ade9dde7eb8ef23fa0e7a6b717a9445c5b498d
Test <Hammertyme101@gmail.com>
2025-04-21 13:26:56 -0400

    logo additem, buy item, login, user and signup initial. app.js mainpage. item model and route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import styled from 'styled-components'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Pcontainer>
+      <P1>Stackz</P1>
+      <P2>Stackz</P2>
+      <P3>Stackz</P3>
+      <P4>Stackz</P4>
+      <P5>Stackz</P5>
+      </Pcontainer>
+      
+
     </div>
   );
 }
 
 export default App;
+
+const Pcontainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  font-size: 20px;
+  font-weight: bold;
+`
+const P1 = styled.p`
+  margin: 0px;
+`
+const P2 = styled.p`
+  margin: -13px;
+  opacity: 70%;
+`
+const P3 = styled.p`
+  margin: -0px;
+  opacity: 50%;
+`
+const P4 = styled.p`
+  margin: -13px;
+  opacity: 30%;
+`
+const P5 = styled.p`
+  margin: -0px;
+  opacity: 10%;
+`

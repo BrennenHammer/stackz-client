b894085de8621e46453de0d9bb24a761af75e395
Test <Hammertyme101@gmail.com>
2025-05-15 13:04:40 -0400

    added jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import ProfilePage from "./pages/ProfilePage";
-import AddItems from "./pages/addItems";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import AddItems from "./pages/addItems.jsx";
 function App() {
   return (
     <>

ba61a061d1aaff34f862587ae5058979e52624e0
Test <Hammertyme101@gmail.com>
2025-05-19 19:26:43 -0400

    new backend link in api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://stackzstore.netlify.app/', // Replace with your deployed API server URL
+  baseURL: 'https://stackzstore.netlify.app/api', // Replace with your deployed API server URL
 });
 
 export default api;

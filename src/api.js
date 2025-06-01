fe4c7f234a055baaa3b8584cbec1b496b6cc8bb2
Test <Hammertyme101@gmail.com>
2025-06-01 18:37:39 -0400

    /api in api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_API_URL
-    : "http://localhost:5000";
+const baseURL = process.env.NODE_ENV === "production"
+  ? process.env.REACT_APP_API_URL
+  : "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL,
